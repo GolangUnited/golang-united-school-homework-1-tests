bcf91b59cb69e6bab65975e3440181dea02d16d2
Serg <rakoth.ru@gmail.com>
2022-12-15 19:46:42 +0500

    Update solution_test.go
    simplified check

--- solution_test.go
+++ solution_test.go
@@ -1,15 +1,15 @@
 package solution
 
 import (
-	"strings"
+	"reflect"
 	"testing"
 )
 
-func TestGetMessage(t *testing.T) {
-	expected := string([]rune{72, 101, 108, 108, 111, 32, 240, 159, 151, 186, 239, 184, 143, 32})
-	msg := GetMessage()
+func Test_GetMessage(t *testing.T) {
+	expected := []byte{72, 101, 108, 108, 111, 32, 240, 159, 151, 186, 239, 184, 143, 32}
+	msg := []byte(GetMessage())
 
-	if !strings.EqualFold(msg, expected) {
+	if !reflect.DeepEqual(expected, msg) {
 		t.Errorf("Unexpected result:\n\tExpected: %q\n\tGot: %q", expected, msg)
 	}
 }
