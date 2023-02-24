732105634b092aae26f2cd59dc1aa48e528a7893
Serge <rakoth.android@gmail.com>
2023-02-24 21:28:05 +0500

    [] test

---
+++ io_reader_test.go
@@ -0,0 +1,143 @@
+package homework_test
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/abdukhashimov/io_homework/homework"
+)
+
+func TestGoalParsers(t *testing.T) {
+	type args struct {
+		strReader *strings.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{
+				strings.NewReader("G()(al)"),
+			},
+			want: "Goal",
+		},
+		{
+			name: "",
+			args: args{
+				strings.NewReader("G()()()()(al)"),
+			},
+			want: "Gooooal",
+		},
+		{
+			name: "",
+			args: args{
+				strings.NewReader("(al)G(al)()()G"),
+			},
+			want: "alGalooG",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := homework.GoalParsers(tt.args.strReader); got != tt.want {
+				t.Errorf("GoalParsers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReaderSplit(t *testing.T) {
+	type args struct {
+		strReader *strings.Reader
+		n         int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "",
+			args: args{
+				strReader: strings.NewReader("helloworld"),
+				n:         2,
+			},
+			want: []string{
+				"he", "ll", "ow", "or", "ld",
+			},
+		},
+		{
+			name: "",
+			args: args{
+				strReader: strings.NewReader("GolangUnitedSchool"),
+				n:         5,
+			},
+			want: []string{
+				"Golan", "gUnit", "edSch", "ool",
+			},
+		},
+		{
+			name: "",
+			args: args{
+				strReader: strings.NewReader("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+				n:         6,
+			},
+			want: []string{
+				"ABCDEF", "GHIJKL", "MNOPQR", "STUVWX", "YZ",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := homework.ReaderSplit(tt.args.strReader, tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				fmt.Println(got, tt.want)
+				t.Errorf("ReaderSplit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReadHalfOfString(t *testing.T) {
+	type args struct {
+		strReader *strings.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "",
+			args: args{
+				strings.NewReader("HELLO-WORLD"),
+			},
+			want: "-WORLD",
+		},
+		{
+			name: "",
+			args: args{
+				strings.NewReader("HELLOWORLD"),
+			},
+			want: "WORLD",
+		},
+		{
+			name: "",
+			args: args{
+				strings.NewReader("GOLANG-UNITED"),
+			},
+			want: "-UNITED",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := homework.SeekTillHalfOfString(tt.args.strReader); got != tt.want {
+				t.Errorf("SeekTillHalfOfString = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
