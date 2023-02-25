ebc6614e0e8eb8b28469c582a855f7cc7c4d62c6
Serg <rakoth.ru@gmail.com>
2023-02-25 11:45:44 +0500

    Update io_reader_test.go

--- io_reader_test.go
+++ io_reader_test.go
@@ -9,47 +9,6 @@ import (
 	"github.com/abdukhashimov/io_homework/homework"
 )
 
-func TestGoalParsers(t *testing.T) {
-	type args struct {
-		strReader *strings.Reader
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "",
-			args: args{
-				strings.NewReader("G()(al)"),
-			},
-			want: "Goal",
-		},
-		{
-			name: "",
-			args: args{
-				strings.NewReader("G()()()()(al)"),
-			},
-			want: "Gooooal",
-		},
-		{
-			name: "",
-			args: args{
-				strings.NewReader("(al)G(al)()()G"),
-			},
-			want: "alGalooG",
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := homework.GoalParsers(tt.args.strReader); got != tt.want {
-				t.Errorf("GoalParsers() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestReaderSplit(t *testing.T) {
 	type args struct {
 		strReader *strings.Reader
